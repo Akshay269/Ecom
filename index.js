9fa0984f964fcdc889bb27da45bde515684dbc37
Akshay <aksrahangdale@gmail.com>
2023-01-26 00:08:26 +0530

    Akshayecom

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const userroute = require("./routes/user.js");
+const authroute = require("./routes/auth.js");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+main().catch((err) => console.log(err));
+
+mongoose.set("strictQuery", true);
+async function main() {
+  await mongoose.connect(process.env.MONGO_URI);
+  console.log("DB Connected");
+}
+
+app.use(express.json());
+app.use("/api/auth", authroute);
+app.use("/api/users", userroute);
+
+app.listen(5000, () => {
+  console.log("Backend Server running ");
+});
