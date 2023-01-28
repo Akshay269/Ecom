3f5521161d06c10816fe363435bf04a914d781b4
Akshay <aksrahangdale@gmail.com>
2023-01-28 21:21:52 +0530

    realshit

--- routes/user.js
+++ routes/user.js
@@ -49,4 +49,23 @@ router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+// get all users
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  const query = req.query.new;
+  try {
+    const users = query
+      ? await User.find().sort({ _id: -1 }).limit(5)
+      : await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//get user stats
+router.get("/stats",verifyTokenAndAdmin,async (req,res)=>{
+
+})
+
 module.exports = router;
